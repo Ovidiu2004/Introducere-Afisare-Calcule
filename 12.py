d0e8a1662395770f193ee7c1e9ebccb8cb3dc7ec
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2020-10-11 12:27:58 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,5 @@
+a=int(input("numarul de oua care  face prima gaina intr-o zi ="))
+b=int(input("numarul de oua care  face a doua gaina intr-o zi ="))
+c=int(input("numarul de oua care  face a treia gaina intr-o zi ="))
+d=int(input("numarul de oua care  face a patra gaina intr-o zi ="))
+print("intr-o saptamina gainile vor face  ",7*(a+b+c+d)," de oua")
---
+++ 7.py
@@ -0,0 +1,3 @@
+v=int(input("dati varsta Mariei"))
+print("greutatea perfecta a Mariei este de",2*v+8,"kg")
+print("inaltimea perfecta a Mariei este de",5*v+80,"cm")
